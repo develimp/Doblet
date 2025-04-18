a595b4dc3530161b8280160361542ffbbcbc4898
Ivan Mas <ivmaspre@hotmail.com>
2025-04-18 21:54:47 +0200

    feat: :sparkles: refs #0034 add memberStatusLog table and update triggers for member status logging

--- db/triggers/member_afterInsert.sql
+++ db/triggers/member_afterInsert.sql
@@ -5,4 +5,9 @@ AFTER INSERT
 ON `member` FOR EACH ROW
 BEGIN
 	CALL insertBalance(NEW.id);
+
+	IF NEW.isRegistered = 1 THEN
+    	INSERT INTO memberStatusLog(memberFk, status)
+    		VALUES (NEW.id, 1);
+  	END IF;
 END
--- db/triggers/member_afterUpdate.sql
+++ db/triggers/member_afterUpdate.sql
@@ -9,6 +9,11 @@ BEGIN
 	END IF;
 	IF OLD.isRegistered = 1 AND NEW.isRegistered = 0 THEN
 		DELETE FROM balance
-		WHERE memberFk = NEW.id;
+			WHERE memberFk = NEW.id;
 	END IF;
+
+	IF OLD.isRegistered <> NEW.isRegistered THEN
+    	INSERT INTO memberStatusLog(memberFk, status)
+    		VALUES (NEW.id, NEW.isRegistered);
+  	END IF;
 END
